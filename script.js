fcdcad20c71b6cebd868e8d7de487a2ccba31119
l0hitk <127608339+l0hitk@users.noreply.github.com>
2024-04-24 02:14:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
 console.log('====================================');
 console.log("Connected");
 console.log('====================================');
+function myFunction(imgs) {
+    var expandImg = document.getElementById("expandedImg");
+    var imgText = document.getElementById("imgtext");
+    expandImg.src = imgs.src;
+    imgText.innerHTML = imgs.alt;
+    expandImg.parentElement.style.display = "block";
+}
+
+const minusButton = document.querySelector('.quantity-button.minus');
+const plusButton = document.querySelector('.quantity-button.plus');
+const quantityInput = document.querySelector('#quantity');
+
+minusButton.addEventListener('click', () => {
+    const currentQuantity = parseInt(quantityInput.value);
+    if (currentQuantity > 1) {
+        quantityInput.value = currentQuantity - 1;
+    }
+});
+
+plusButton.addEventListener('click', () => {
+    const currentQuantity = parseInt(quantityInput.value);
+    quantityInput.value = currentQuantity + 1;
+});
